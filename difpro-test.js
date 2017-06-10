560d6696416ae2970f064e08c3688e0cce90e6e0
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-10 13:12:36 +0800

    Updated test.

--- difpro-test.js
+++ difpro-test.js
@@ -5,4 +5,7 @@ const difpro = require( "./difpro.js" );
 assert.deepEqual( difpro( { "test": "123", "hello": "world" }, { "hello": "world", "apple": "orange" } ),
 	[ "test", "apple" ], "should be deeply equal" );
 
+let test1 = difpro( { [ Symbol.for( "hello" ) ]: "hello" }, { [ Symbol.for( "hello" ) ]: "hello", [ Symbol.for( "hi" ) ]: "hi" } );
+assert.deepEqual( test1, [ Symbol.for( "hi" ) ], "should be deeply equal" );
+
 console.log( "ok" );
--- difpro.support.js
+++ difpro.support.js
@@ -35,6 +35,9 @@
               			"module": "difpro",
               			"author": "Richeve S. Bebedor",
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/difpro.git",
               			"test": "difpro-test.js",
               			"global": true
